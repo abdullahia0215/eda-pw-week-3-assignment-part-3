582752ac147e979b63adbf16b154d874322996de
Abdi <abdullahia0215@gmail.com>
2024-04-11 22:29:43 -0500

    stretch goals complete

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -46,13 +46,13 @@ console.log("6. Showing supplyChanges...");
 for (let i = 0; i < supplyChanges.length; i++) {
   console.log(supplyChanges[i]);
   if (supplyChanges[i] > 0) {
-    console.log("Amount added:  " + supplyChanges[i]);
+    console.log("added:  " + supplyChanges[i]);
   }
   if (supplyChanges[i] === 0) {
-    console.log("No changes made.");
+    console.log("No change");
   }
   if (supplyChanges[i] < 0) {
-    console.log("Amount removed: " + supplyChanges[i]);
+    console.log("removed: " + supplyChanges[i]);
   }
 }
 
@@ -61,9 +61,29 @@ console.log("---  Stretch Goals  ---");
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
+console.log(supplyChanges);
+
+for (let supplyChange of supplyChanges){
+ if (supplyChange > 0){
+  console.log("added " + supplyChange);
+ } else if (supplyChange === 0){
+  console.log("no change")
+ } else {
+  console.log("removed " + (-supplyChange));
+ }
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log("8. Total supplies available is:");
+
+
+let partsAvailable = 0;
+
+for (let change of supplyChanges) {
+  partsAvailable += change; 
+}
+
+console.log("8. Total supplies available is:", partsAvailable);
+
 
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
@@ -72,3 +92,20 @@ console.log("8. Total supplies available is:");
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let totalParts = 572;
+let partsPerBox = 7;
+let boxesFilled = 0;
+let remainingParts = totalParts;
+
+while (remainingParts > 0) {
+  if (remainingParts >= partsPerBox) {
+    boxesFilled++; 
+    remainingParts -= partsPerBox; 
+  } else {
+    break;
+  }
+};
+
+console.log("boxes filled:", boxesFilled);
+console.log("parts remaining:", remainingParts);
